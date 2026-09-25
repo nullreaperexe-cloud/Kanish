9727ff1c4dd2579a4de24a7b1257fec28202e735
nullreaperexe-cloud <nullreaper.exe@gmail.com>
2026-09-25 16:49:58 +0530

    Use stable Android API 36 for cloud build

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 
 android {
     namespace = "com.classping.app"
-    compileSdk = 37
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.classping.app"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
