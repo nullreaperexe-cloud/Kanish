4b49425838d03b6a7cee5d576d03a096e08063e7
nullreaperexe-cloud <nullreaper.exe@gmail.com>
2026-09-25 16:52:04 +0530

    Migrate app module to AGP 9 built-in Kotlin

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.gms.google-services")
 }
@@ -18,7 +17,6 @@ android {
     }
     buildFeatures { compose = true; buildConfig = true }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
-    kotlinOptions { jvmTarget = "17" }
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
 }
 
